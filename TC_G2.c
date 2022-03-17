73f808caada91090ab798fa8bc7e48c950fd6c44
DavidFerH <davidferreroherrera@gmail.com>
2022-03-17 08:56:11 +0100

    Proyecto finalizado

--- TC_G2.c
+++ TC_G2.c
@@ -7,74 +7,85 @@
 #define b 4
 
 // Prototipos de las funciones
-void notaFinal(float notas[a][b]);
+void notaFinal(float notas[a][b],int aPonderacion[b]);
 void maxMinMedia(float notas[a][b]);
 void listPruebasNotas(float notas[a][b]);
-void cambiarPonderacion(float notas[a][b]);
+void cambiarPonderacion(int arrayPonderaciones[b]);
 
 // Funcion main
 void main() {
     // Declaracion de las variables
     float aNotas[a][b];
     int seleccion;
+    int aPonderacion[b] = {25, 25, 25, 25};
+
+
 
     // Insercion de las notas
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 4; j++) {
-            printf("Introduzca la nota %d del alumno %d: ", j + 1, i + 1);
-            scanf("%f", &aNotas[i][j]);
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < b; j++) {
+            do {
+                printf("Introduzca la nota %d del alumno %d: ", j + 1, i + 1);
+                scanf("%f", &aNotas[i][j]);
+
+                if (aNotas[i][j] < 0 ||aNotas[i][j] > 10) {
+                    printf("El valor de la nota debe estar entre 0 y 10\n");
+                }
+                
+            } while(aNotas[i][j] < 0 || aNotas[i][j] > 10);
         }
     }
 
-    // Muestra por pantalla de las distintas opciones
-    printf("\n--- MENU DE SELECCION ---\n1- Nota final por alumno\n2- Notas maxima, minima y media por prueba\n3- Listado de pruebas y notas\n4- Modificacion de ponderacion de notas\n");
-    scanf("%d", &seleccion);
+    do {
+        // Muestra por pantalla de las distintas opciones
+        printf("\n--- MENU DE SELECCION ---\n1- Nota final por alumno\n2- Notas maxima, minima y media por prueba\n3- Listado de pruebas y notas\n4- Modificacion de ponderacion de notas\n5- Salir del programa\n");
+        scanf("%d", &seleccion);
 
-    // Switch de seleccion de las diferentes opciones
-    switch(seleccion) {
-    case 1:
-        notaFinal(aNotas);
-        break;
-    
-    case 2:
-        maxMinMedia(aNotas);
-        break;
+        // Switch de seleccion de las diferentes opciones
+        switch(seleccion) {
+        case 1:
+            notaFinal(aNotas, aPonderacion);
+            break;
+        
+        case 2:
+            maxMinMedia(aNotas);
+            break;
 
-    case 3:
-        listPruebasNotas(aNotas);
-        break;
+        case 3:
+            listPruebasNotas(aNotas);
+            break;
 
-    case 4:
-        cambiarPonderacion(aNotas);
-        break;
-
-    default:
-        printf("La opcion seleccionada no es valida");
-        break;
-    }
-
-    return;
+        case 4:
+            cambiarPonderacion(aPonderacion);
+            break;
+        case 5:
+            break;
+        default:
+            printf("La opcion seleccionada no es valida");
+            break;
+        }
+    } while(seleccion != 5);
 }
 
-void notaFinal(float notas[a][b]) {
+void notaFinal(float notas[a][b], int aPonderacion[b]) {
     float suma = 0;
 
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 4; j++) {
-            suma = suma + notas[i][j];
+    for(int i = 0; i < a; i++) {
+        for(int j = 0; j < b; j++) {
+            suma = suma + (notas[i][j] * (aPonderacion[j] / 100));
         }
-        printf("\nLa media del alunno %d es: %.2f", i + 1, (suma / b));
+        printf("\nLa media del alumno %d es: %.2f", i + 1, (suma));
         suma = 0;
     }
-
-    return;
 }
 
 void maxMinMedia(float notas[a][b]) {
     float max = 0, min = 0, suma = 0;
 
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 10; j++) {
+    for(int i = 0; i < b; i++) {
+        min = notas[0][i];
+        max = 0;
+        for(int j = 0; j < a; j++) {
             suma = suma + notas[j][i];
 
             if(notas[j][i] > max) {
@@ -93,16 +104,14 @@ void maxMinMedia(float notas[a][b]) {
         min = 0;
         suma = 0;
     }
-
-    return;
 }
 
 void listPruebasNotas(float notas[a][b]) {
     int aprobados = 0;
 
-    for(int i = 0; i < 4; i++) {
+    for(int i = 0; i < b; i++) {
         printf("Listado de notas de la prueba %d", i + 1);
-        for(int j = 0; j < 10; j++) {
+        for(int j = 0; j < a; j++) {
             printf("\nAlumno %d: %.2f", j + 1, notas[j][i]);
             if(notas[j][i] >= 5) {
                 aprobados++;
@@ -111,45 +120,36 @@ void listPruebasNotas(float notas[a][b]) {
         printf("\nEl numero de aprobados en la prueba %d es de %d", i + 1, aprobados);
         printf("\nEl numero de suspensos en la prueba %d es de %d", i + 1, a - aprobados);
         printf("\n");
+
         aprobados = 0;
     }
 }
 
-// Esta parte no funciona
 
-void cambiarPonderacion(float notas[a][b]) {
-    int arrayPonderaciones[4] = {25, 25, 25, 25}, suma[4];
+//Metodo para cambiar la ponderacion con los valores introducidos por el usuario
+void cambiarPonderacion(int arrayPonderaciones[]) {
 
     do {
-        printf("Introduzca las nuevas ponderacion (entre 10 y 60)");
+        printf("Introduzca las nuevas ponderacion (entre 10 y 60)\n");
         scanf("%d %d %d %d", &arrayPonderaciones[0], &arrayPonderaciones[1], &arrayPonderaciones[2], &arrayPonderaciones[3]);
 
-        if(
+        if ( //comprobacion de valor entre 10 y 60
             arrayPonderaciones[0] < 10 || arrayPonderaciones[0] > 60 ||
             arrayPonderaciones[1] < 10 || arrayPonderaciones[1] > 60 ||
             arrayPonderaciones[2] < 10 || arrayPonderaciones[2] > 60 ||
             arrayPonderaciones[3] < 10 || arrayPonderaciones[3] > 60
         ) {
-            printf("La ponderacion introducida no es valida");
+            printf("La ponderacion introducida no es valida\n");
+        } else if (arrayPonderaciones[0] + arrayPonderaciones[1] + arrayPonderaciones[2] + arrayPonderaciones[3] != 100) { //comprobar que la suma de ponderaciones sea 100
+            printf("La suma de las ponderaciones no es valida\n");
         }
 
-    } while (
-        arrayPonderaciones[0] >= 10 || arrayPonderaciones[0] <= 60 ||
-        arrayPonderaciones[1] >= 10 || arrayPonderaciones[1] <= 60 ||
-        arrayPonderaciones[2] >= 10 || arrayPonderaciones[2] <= 60 ||
-        arrayPonderaciones[3] >= 10 || arrayPonderaciones[3] <= 60
+    } while (//bucle para comprobar que el valor de ponderaciones esta entre 10 y 60 y su suma da 100
+        arrayPonderaciones[0] < 10 || arrayPonderaciones[0] > 60 ||
+        arrayPonderaciones[1] < 10 || arrayPonderaciones[1] > 60 ||
+        arrayPonderaciones[2] < 10 || arrayPonderaciones[2] > 60 ||
+        arrayPonderaciones[3] < 10 || arrayPonderaciones[3] > 60 ||
+        (arrayPonderaciones[0] + arrayPonderaciones[1] + arrayPonderaciones[2] + arrayPonderaciones[3] != 100)
     );
     
-
-
-    if(arrayPonderaciones[0] + arrayPonderaciones[1] + arrayPonderaciones[2] + arrayPonderaciones[3] != 100) {
-        printf("La suma de las ponderaciones no es valida");
-    } else {
-        for(int i = 0; i < 10; i++) {
-            for(int j = 0; j < 4; j++) {
-                suma[j] = notas[i][j];
-            }
-        printf("\nLa media del alumno %d es: %.2f", i + 1, (suma[0] * (arrayPonderaciones[0] / 100)) + (suma[1] * (arrayPonderaciones[1] / 100)) + (suma[2] * (arrayPonderaciones[2] / 100)) + (suma[3] * (arrayPonderaciones[3] / 100)));
-    }
-    }
 }
